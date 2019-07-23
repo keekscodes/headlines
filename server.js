fc797787241a45fde0432c9ed12641293f053086
Kimberly Mueller <kamueller2@gmail.com>
2019-07-23 18:45:13 -0400

    setting up paths

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ var mongojs = require("mongojs");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+app.use(express.static("public"));
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
+
 // Initialize Express
 var app = express();
 
 app.use(express.static("public"));
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
 
 // Database configuration
 var databaseUrl = "scraper";
@@ -17,23 +33,65 @@ var collections = ["scrapedData"];
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
-  console.log("Database Error:", error);
+    console.log("Database Error:", error);
 });
 
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-  res.send("Hello world");
+    res.send("Hello world");
 });
 
 app.get("/all", function(req, res) {
-  db.scrapedData.find({}, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
-  })
-  
+    db.scrapedData.find({}, function(error, found) {
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise, send the result of this query to the browser
+        else {
+            res.json(found);
+        }
+    })
+
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+    // Make a request via axios for the news section of `ycombinator`
+    axios.get("https://www.petmd.com/dog/centers/care/content/health/all").then(function(response) {
+        // Load the html body from axios into cheerio
+        var $ = cheerio.load(response.data);
+        // For each element with a "title" class
+        $("h2.a").each(function(i, element) {
+            // Save the text and href of each link enclosed in the current element
+            var title = $(element).children("a").text();
+            var link = $(element).children("a").attr("href");
+
+            // If this found element had both a title and a link
+            if (title && link) {
+                // Insert the data in the scrapedData db
+                db.scrapedData.insert({
+                        title: title,
+                        link: link
+                    },
+                    function(err, inserted) {
+                        if (err) {
+                            // Log the error if one is encountered during the query
+                            console.log(err);
+                        } else {
+                            // Otherwise, log the inserted data
+                            console.log(inserted);
+                        }
+                    });
+            }
+        });
+    });
+
+    // Send a "Scrape Complete" message to the browser
+    res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
 });
