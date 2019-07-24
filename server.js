6cfe393dea04d8463ccab27d7f837f43b58e5b94
Kimberly <kamueller2@gmail.com>
2019-07-23 22:46:22 -0400

    dependencies and connecting to server

--- server.js
+++ server.js
@@ -1,97 +1,56 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-app.use(express.static("public"));
-
 var exphbs = require('express-handlebars');
-app.engine("handlebars", exphbs({
-    defaultLayout: "main",
-    partialsDir: path.join(__dirname, "/views/layouts/partials")
-}));
-app.set("view engine", "handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
 
-// Initialize Express
+
+var PORT =  process.env.PORT || 3000;
+
+// Initialize the Express Server
 var app = express();
 
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 app.use(express.static("public"));
 
-var exphbs = require('express-handlebars');
-app.engine("handlebars", exphbs({
-    defaultLayout: "main",
-    partialsDir: path.join(__dirname, "/views/layouts/partials")
-}));
-app.set("view engine", "handlebars");
+
+//Handlebars
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var databaseUrl = "newsDB";
+var collections = ["articles"];
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-    console.log("Database Error:", error);
-});
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.ObjectId;
 
-// Main route (simple Hello World Message)
+var articles = new Schema({
+  Author: ObjectId,
+  Headline: String,
+  Summary: String,
+  URL: String
+})
+
+// Routes
 app.get("/", function(req, res) {
-    res.send("Hello world");
+    res.render("index", {});
+  })
+  
+  app.get("/api/articles", function(req, res) {
+    db.articles.find({})
+  })
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
 
-app.get("/all", function(req, res) {
-    db.scrapedData.find({}, function(error, found) {
-        if (error) {
-            console.log(error);
-        }
-        // Otherwise, send the result of this query to the browser
-        else {
-            res.json(found);
-        }
-    })
-
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-    // Make a request via axios for the news section of `ycombinator`
-    axios.get("https://www.petmd.com/dog/centers/care/content/health/all").then(function(response) {
-        // Load the html body from axios into cheerio
-        var $ = cheerio.load(response.data);
-        // For each element with a "title" class
-        $("h2.a").each(function(i, element) {
-            // Save the text and href of each link enclosed in the current element
-            var title = $(element).children("a").text();
-            var link = $(element).children("a").attr("href");
-
-            // If this found element had both a title and a link
-            if (title && link) {
-                // Insert the data in the scrapedData db
-                db.scrapedData.insert({
-                        title: title,
-                        link: link
-                    },
-                    function(err, inserted) {
-                        if (err) {
-                            // Log the error if one is encountered during the query
-                            console.log(err);
-                        } else {
-                            // Otherwise, log the inserted data
-                            console.log(inserted);
-                        }
-                    });
-            }
-        });
-    });
-
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
-});
-
-
-// Listen on port 3000
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
-});
+module.exports=app;
